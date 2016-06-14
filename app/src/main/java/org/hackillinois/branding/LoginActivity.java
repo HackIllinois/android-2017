a2284655aacbdf97366498a0cd151378d3b6372b
tommypacker <tommypacker@gmail.com>
2016-06-13 21:44:15 -0700

    Continue implementing login

--- app/src/main/java/org/hackillinois/branding/LoginActivity.java
+++ app/src/main/java/org/hackillinois/branding/LoginActivity.java
@@ -1,11 +1,14 @@
 package org.hackillinois.branding;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 /**
  * Created by tommypacker for HackIllinois' 2016 Clue Hunt
@@ -15,11 +18,16 @@ public class LoginActivity extends AppCompatActivity {
     private Button loginButton;
     private EditText emailField;
     private EditText passwordField;
+    private SharedPreferences sharedPreferences;
+    private SharedPreferences.Editor editor;
 
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity);
+
+        sharedPreferences = this.getSharedPreferences("settings", Context.MODE_PRIVATE);
+        editor = sharedPreferences.edit();
 
         emailField = (EditText) findViewById(R.id.emailField);
         passwordField = (EditText) findViewById(R.id.passwordField);
@@ -33,7 +41,17 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     private void authorize(String email, String password){
+        if(email.equals("hackillinois") && password.equals("2017")){
+            moveOn();
+        }else{
+            Toast.makeText(getApplicationContext(), "Sorry, Incorrect Credentials", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void moveOn(){
+        editor.putBoolean("hasAuthed", true).apply();
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
+        this.finish();
     }
 }
