ab76a12a0d134538ab7e1badaee4499961e21242
tommypacker <tommypacker@gmail.com>
2016-06-15 20:28:39 -0700

    Very basic splash screen implemented

--- app/src/main/java/org/hackillinois/app2017/Login/SplashScreen.java
+++ app/src/main/java/org/hackillinois/app2017/Login/SplashScreen.java
@@ -1,10 +1,37 @@
 package org.hackillinois.app2017.Login;
 
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import org.hackillinois.app2017.R;
 
 /**
  * Created by tommypacker for HackIllinois' 2016 Clue Hunt
  */
 public class SplashScreen extends AppCompatActivity {
 
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_splash);
+        setTitle("HackIllinois 2017");
+
+        Button splashButton = (Button) findViewById(R.id.splashButton);
+        if (splashButton != null) {
+            splashButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    moveOn();
+                }
+            });
+        }
+    }
+
+    private void moveOn(){
+        Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+        startActivity(intent);
+    }
 }
