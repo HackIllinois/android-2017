68d56f1b50c27afc4f887b6f564b0ff859d1a593
tommypacker <tommypacker@gmail.com>
2016-06-16 22:57:18 -0700

    Set alternative theme for splash screen

--- app/src/main/java/org/hackillinois/app2017/Login/LoginActivity.java
+++ app/src/main/java/org/hackillinois/app2017/Login/LoginActivity.java
@@ -57,7 +57,6 @@ public class LoginActivity extends AppCompatActivity {
         switch (item.getItemId()) {
             case android.R.id.home:
                 // this takes the user 'back', as if they pressed the left-facing triangle icon on the main android toolbar.
-                // if this doesn't work as desired, another possibility is to call `finish()` here.
                 onBackPressed();
                 return true;
             default:
