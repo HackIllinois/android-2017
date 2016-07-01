bdd77ce7568b0d85bd59b39df5ac8cf09d248f2e
tommypacker <tommypacker@gmail.com>
2016-06-30 20:45:40 -0700

    Added ticket model to helpq

--- app/src/main/java/org/hackillinois/app2017/HelpQ/NewTicketActivity.java
+++ app/src/main/java/org/hackillinois/app2017/HelpQ/NewTicketActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import org.hackillinois.app2017.R;
 
@@ -11,6 +13,8 @@ import org.hackillinois.app2017.R;
  * Created by tommypacker for HackIllinois' 2016 Clue Hunt
  */
 public class NewTicketActivity extends AppCompatActivity {
+
+    private Button submitTicketButton;
 
     @Override
     public void onCreate(Bundle savedInstanceState){
@@ -21,6 +25,14 @@ public class NewTicketActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle("New Ticket");
+
+        submitTicketButton = (Button) findViewById(R.id.submitTicket);
+        submitTicketButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 
     @Override
---
+++ app/src/main/java/org/hackillinois/app2017/HelpQ/Ticket.java
@@ -0,0 +1,29 @@
+package org.hackillinois.app2017.HelpQ;
+
+/**
+ * Created by tommypacker for HackIllinois' 2016 Clue Hunt
+ */
+public class Ticket {
+
+    private String userName;
+    private String issue;
+    private String phoneNumber;
+
+    public Ticket(String userName, String issue, String phoneNumber){
+        this.userName = userName;
+        this.issue = issue;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getUserName(){
+        return userName;
+    }
+
+    public String getIssue(){
+        return issue;
+    }
+
+    public String getPhoneNumber(){
+        return phoneNumber;
+    }
+}
