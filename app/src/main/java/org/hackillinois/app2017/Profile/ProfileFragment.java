1f68be95db8d431bc14d97553d31fdf69f10c318
tommypacker <tommypacker@gmail.com>
2016-09-12 23:06:41 -0500

    Add onclick listeners to profile page + ui updates to announcements

--- app/src/main/java/org/hackillinois/app2017/Profile/ProfileFragment.java
+++ app/src/main/java/org/hackillinois/app2017/Profile/ProfileFragment.java
@@ -1,10 +1,14 @@
 package org.hackillinois.app2017.Profile;
 
+import android.content.Intent;
+import android.media.Image;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import org.hackillinois.app2017.R;
 
@@ -13,10 +17,45 @@ import org.hackillinois.app2017.R;
  */
 public class ProfileFragment extends Fragment {
 
+    private View.OnClickListener listener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            int id = v.getId();
+            switch (id){
+                case R.id.linkedin_link:
+                    openLink("http://www.linkedin.com");
+                    break;
+                case R.id.github_link:
+                    openLink("http://github.com");
+                    break;
+                case R.id.resume_link:
+                    openLink("resume");
+                    break;
+                case R.id.portfolio_link:
+                    openLink("portfolio");
+                    break;
+            }
+        }
+    };
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState){
         View view = inflater.inflate(R.layout.layout_profile, parent, false);
 
+        ImageView linkedin = (ImageView) view.findViewById(R.id.linkedin_link);
+        linkedin.setOnClickListener(listener);
+        ImageView github = (ImageView) view.findViewById(R.id.github_link);
+        github.setOnClickListener(listener);
+        ImageView resume = (ImageView) view.findViewById(R.id.resume_link);
+        resume.setOnClickListener(listener);
+        ImageView portfolio = (ImageView) view.findViewById(R.id.portfolio_link);
+        portfolio.setOnClickListener(listener);
+
         return view;
+    }
+
+    private void openLink(String link){
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(link));
+        startActivity(browserIntent);
     }
 }
