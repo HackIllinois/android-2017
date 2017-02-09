dc45e3a536988e16368793e4280528b309b0909d
dl-eric <orangepamelo@gmail.com>
2017-02-08 20:33:54 -0600

    Add auth endpoint

--- app/src/main/java/org/hackillinois/app2017/Backend/APIHelper.java
+++ app/src/main/java/org/hackillinois/app2017/Backend/APIHelper.java
@@ -2,4 +2,6 @@ package org.hackillinois.app2017.Backend;
 
 public class APIHelper {
     public static String serverAddress = "https://api.hackillinois.org/";
+
+    public static String authEndpoint = serverAddress + "/v1/auth";
 }
