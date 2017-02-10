c9c39685e509a2a10578a2896c5530d998dc6a65
dl-eric <orangepamelo@gmail.com>
2017-02-09 22:13:36 -0600

    Fix URL

--- app/src/main/java/org/hackillinois/app2017/Backend/APIHelper.java
+++ app/src/main/java/org/hackillinois/app2017/Backend/APIHelper.java
@@ -1,7 +1,7 @@
 package org.hackillinois.app2017.Backend;
 
 public class APIHelper {
-    public static String serverAddress = "https://api.hackillinois.org/";
+    public static String serverAddress = "https://api.hackillinois.org";
 
     public static String authEndpoint = serverAddress + "/v1/auth";
 }
