4aa670dbb87e66aedd4e42499c7bbeb9f1b8c561
dl-eric <orangepamelo@gmail.com>
2017-02-12 13:59:38 -0600

    Add test instance url

--- app/src/main/java/org/hackillinois/app2017/Backend/APIHelper.java
+++ app/src/main/java/org/hackillinois/app2017/Backend/APIHelper.java
@@ -5,5 +5,6 @@ public class APIHelper {
 
     public static String authEndpoint = serverAddress + "/v1/auth";
     public static String userEndpoint = serverAddress + "/v1/registration/attendee";
-    public static String eventsEndpoint = serverAddress + "/v1/events";
+    //public static String eventsEndpoint = serverAddress + "/v1/events";
+    public static String eventsEndpoint = "http://13.90.146.188:8080/v1/events";
 }
