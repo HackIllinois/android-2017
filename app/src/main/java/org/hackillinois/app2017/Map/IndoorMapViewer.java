34da3511adf3fa4972f353dfafb6ac17c0764f05
Kevin Hinterlong <kevinhinterlong@gmail.com>
2017-02-23 22:04:39 -0600

    Get list of Images for indoor buildings

--- app/src/main/java/org/hackillinois/app2017/Map/IndoorMapViewer.java
+++ app/src/main/java/org/hackillinois/app2017/Map/IndoorMapViewer.java
@@ -4,10 +4,13 @@ import android.app.Activity;
 import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.widget.RecyclerView;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 import org.hackillinois.app2017.R;
 
@@ -15,6 +18,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 /**
@@ -23,19 +27,27 @@ import butterknife.ButterKnife;
 
 public class IndoorMapViewer extends Activity {
     public static String BUILDING_NAME = "BUILDING_NAME";
+    @BindView(R.id.indoor_map_viewer) RelativeLayout indoorMap;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.);
+        setContentView(R.layout.layout_indoor_map_viewer);
         ButterKnife.bind(this);
 
         Buildings building = getBuildingEnum();
         List<ImageView> listOfImages = getBuildingImages(building);
+
     }
 
-    public Intent showMap(Activity current, Buildings building) {
+    public static Intent showMap(Activity current, Buildings building) {
         Intent intent = new Intent(current.getApplicationContext(), current.getClass());
+        intent.putExtra(BUILDING_NAME,building.toString());
+        return intent;
+    }
+
+    public static Intent showMap(Fragment current, Buildings building) {
+        Intent intent = new Intent(current.getContext(), current.getClass());
         intent.putExtra(BUILDING_NAME,building.toString());
         return intent;
     }
@@ -44,16 +56,16 @@ public class IndoorMapViewer extends Activity {
         List<Integer> ids = new ArrayList<>();
         switch (building) {
             case SIEBEL:
-                ids = Arrays.asList();
+                ids = Arrays.asList(R.drawable.siebel_careerfair, R.drawable.siebel_basement,R.drawable.siebel_firstfloor,R.drawable.siebel_secondfloor,R.drawable.siebel_thirdfloor,R.drawable.siebel_fourthfloor);
                 break;
             case ECEB:
-                ids = Arrays.asList();
+                ids = Arrays.asList(R.drawable.eceb_careerfair, R.drawable.eceb_firstfloor,R.drawable.eceb_secondfloor,R.drawable.eceb_thirdfloor,R.drawable.eceb_fourthfloor);
                 break;
             case DCL:
-                ids = Arrays.asList();
+                ids = Arrays.asList(R.drawable.dcl_firstfloor);
                 break;
             case UNION:
-                ids = Arrays.asList();
+                ids = Arrays.asList(R.drawable.union_firstfloor);
                 break;
         }
         return getDrawables(ids);
