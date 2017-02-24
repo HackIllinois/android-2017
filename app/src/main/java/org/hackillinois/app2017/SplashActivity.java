9fba6ca42de71c4b1514f78f9918b8e5a8f681e3
dl-eric <orangepamelo@gmail.com>
2017-02-23 18:33:30 -0600

    Add splashactivity

---
+++ app/src/main/java/org/hackillinois/app2017/SplashActivity.java
@@ -0,0 +1,20 @@
+package org.hackillinois.app2017;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import org.hackillinois.app2017.Login.LoginActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Intent i = new Intent(this, LoginActivity.class);
+        startActivity(i);
+        finish();
+    }
+}
