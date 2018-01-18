22dedff96749affd966cdae4e83d62ea396ea6d5
dl-eric <orangepamelo@gmail.com>
2018-01-17 23:37:21 -0600

    Create Splash GIF animation

--- app/src/main/java/org/hackillinois/android/SplashActivity.java
+++ app/src/main/java/org/hackillinois/android/SplashActivity.java
@@ -1,13 +1,35 @@
 package org.hackillinois.android;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
-public class SplashActivity extends DrawerActivity {
+import java.io.IOException;
+
+import pl.droidsonroids.gif.GifDrawable;
+
+public class SplashActivity extends HackillinoisActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        try {
+            GifDrawable gif = new GifDrawable(getResources(), R.drawable.appanimation);
+
+            int duration = gif.getDuration();
+
+            new Handler().postDelayed(() -> {
+                Intent i = new Intent(getApplicationContext(), LoginChooserActivity.class);
+                startActivity(i);
+                finish();
+            }, duration);
+
+        } catch (IOException e) {
+            Intent i = new Intent(this, LoginChooserActivity.class);
+            startActivity(i);
+            finish();
+        }
     }
 }
