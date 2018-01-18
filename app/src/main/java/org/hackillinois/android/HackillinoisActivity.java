8c2fe464b5709d70755ba9aa9bc7b774305365d5
Kevin Hinterlong <kevinhinterlong@gmail.com>
2018-01-17 21:20:49 -0600

    Update dependencies and add runtime font config

---
+++ app/src/main/java/org/hackillinois/android/HackillinoisActivity.java
@@ -0,0 +1,14 @@
+package org.hackillinois.android;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+
+import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
+
+
+public class HackillinoisActivity extends AppCompatActivity {
+	@Override
+	protected void attachBaseContext(Context newBase) {
+		super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
+	}
+}
