1e3966a22f54619b5a4f1a96bc68cc5120b07d06
dl-eric <orangepamelo@gmail.com>
2018-01-17 21:26:37 -0600

    Add activities

---
+++ app/src/main/java/org/hackillinois/android/DrawerActivity.java
@@ -0,0 +1,13 @@
+package org.hackillinois.android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+
+public abstract class DrawerActivity extends AppCompatActivity {
+    @Override
+    public void setContentView(final int layoutResID) {
+    }
+
+}
---
+++ app/src/main/java/org/hackillinois/android/SplashActivity.java
@@ -0,0 +1,13 @@
+package org.hackillinois.android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
