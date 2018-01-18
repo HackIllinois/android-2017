b4eb452c2cf1a831d84278e2842af5dfe034771f
dl-eric <orangepamelo@gmail.com>
2018-01-18 00:48:51 -0600

    Fix SplashActivity

--- app/src/main/java/org/hackillinois/android/SplashActivity.java
+++ app/src/main/java/org/hackillinois/android/SplashActivity.java
@@ -3,6 +3,7 @@ package org.hackillinois.android;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 
 import java.io.IOException;
 
@@ -17,19 +18,19 @@ public class SplashActivity extends HackillinoisActivity {
 
         try {
             GifDrawable gif = new GifDrawable(getResources(), R.drawable.appanimation);
-
+            Log.d("Splash", "Hit");
             int duration = gif.getDuration();
 
             new Handler().postDelayed(() -> {
                 Intent i = new Intent(getApplicationContext(), LoginChooserActivity.class);
                 startActivity(i);
+                finish();
             }, duration);
 
         } catch (IOException e) {
             Intent i = new Intent(this, LoginChooserActivity.class);
             startActivity(i);
+            finish();
         }
-
-        finish();
     }
 }
