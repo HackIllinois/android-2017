a9d6c62e7ddc0f0fc749f82168719202a30d7985
Kevin Hinterlong <kevinhinterlong@gmail.com>
2018-02-01 01:08:36 -0600

    Enhance toolbar use with fragments
    - Fragments now set the title when changed
    - hamburger bar successfully appears with color

--- app/src/main/java/org/hackillinois/android/activity/MainActivity.java
+++ app/src/main/java/org/hackillinois/android/activity/MainActivity.java
@@ -1,21 +1,19 @@
 package org.hackillinois.android.activity;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
-import android.util.AttributeSet;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Toast;
 
 import org.hackillinois.android.HomeFragment;
 import org.hackillinois.android.NotificationFragment;
@@ -27,17 +25,15 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MainActivity extends HackillinoisActivity {
-    @BindView(R.id.navigation)
-    NavigationView navigationView;
-
-    @BindView(R.id.drawer)
-    DrawerLayout drawerLayout;
+    @BindView(R.id.navigation) NavigationView navigationView;
+    @BindView(R.id.drawer) DrawerLayout drawerLayout;
+    @BindView(R.id.genericToolbar) Toolbar toolbar;
 
     private FragmentManager fragmentManager;
-    private Fragment mHomeFragment;
-    private Fragment mNotificationFragment;
-    private Fragment mProfileFragment;
-    private Fragment mScheduleFragment;
+    private Fragment homeFragment;
+    private Fragment notificationFragment;
+    private Fragment profileFragment;
+    private Fragment scheduleFragment;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -45,19 +41,35 @@ public class MainActivity extends HackillinoisActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
+        setSupportActionBar(toolbar);
+
+		ActionBarDrawerToggle drawerToggle = new ActionBarDrawerToggle(
+				this,
+				drawerLayout,
+				toolbar,
+				R.string.open_drawer,
+				R.string.close_drawer
+		);
+		drawerToggle.syncState();
+
+
         navigationView.setNavigationItemSelectedListener(item -> {
             switch(item.getItemId()) {
                 case R.id.menu_home:
-                    swapFragments(mHomeFragment);
+                    swapFragments(homeFragment);
+                    getSupportActionBar().setTitle(getString(R.string.menu_home));
                     break;
                 case R.id.menu_profile:
-                    swapFragments(mProfileFragment);
+                    swapFragments(profileFragment);
+                    getSupportActionBar().setTitle(getString(R.string.menu_profile));
                     break;
                 case R.id.menu_notifications:
-                    swapFragments(mNotificationFragment);
+                    swapFragments(notificationFragment);
+                    getSupportActionBar().setTitle(getString(R.string.menu_notifications));
                     break;
                 case R.id.menu_schedule:
-                    swapFragments(mScheduleFragment);
+                    swapFragments(scheduleFragment);
+                    getSupportActionBar().setTitle(getString(R.string.menu_schedule));
                     break;
                 case R.id.menu_map:
                     // TODO: Open Map app
@@ -72,18 +84,18 @@ public class MainActivity extends HackillinoisActivity {
         // Kill LoginChooserActivity so user can't press back and get to it.
         sendBroadcast(new Intent("finish_activity"));
 
-        mHomeFragment = new HomeFragment();
-        mNotificationFragment = new NotificationFragment();
-        mProfileFragment = new ProfileFragment();
-        mScheduleFragment = new ScheduleFragment();
+        homeFragment = new HomeFragment();
+        notificationFragment = new NotificationFragment();
+        profileFragment = new ProfileFragment();
+        scheduleFragment = new ScheduleFragment();
 
         Toolbar toolbar = findViewById(R.id.genericToolbar);
         setSupportActionBar(toolbar);
 
         fragmentManager = getSupportFragmentManager();
 
-        fragmentManager.beginTransaction().replace(R.id.content_frame, mHomeFragment).commit();
-        setTitle("Home");
+        fragmentManager.beginTransaction().replace(R.id.content_frame, homeFragment).commit();
+        getSupportActionBar().setTitle(getString(R.string.menu_home));
     }
 
     private void swapFragments(Fragment fragment) {
