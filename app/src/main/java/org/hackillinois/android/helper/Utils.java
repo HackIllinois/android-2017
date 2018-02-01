81f439b65a245e993784d8e9192b5de74a7a7c02
Raajesh12 <raajstar3@gmail.com>
2018-01-31 23:40:33 -0600

    Got QR code to work for super user

--- app/src/main/java/org/hackillinois/android/helper/Utils.java
+++ app/src/main/java/org/hackillinois/android/helper/Utils.java
@@ -1,8 +1,15 @@
 package org.hackillinois.android.helper;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.support.v4.content.ContextCompat;
+
+import net.glxn.qrgen.android.QRCode;
+
+import java.util.Locale;
 import android.view.Window;
 import android.view.WindowManager;
 
@@ -22,5 +29,13 @@ public class Utils {
 		lp.width = WindowManager.LayoutParams.MATCH_PARENT;
 		lp.height = WindowManager.LayoutParams.MATCH_PARENT;
 		window.setAttributes(lp);
+	}
+
+	public static Bitmap getQRCodeBitmap(Context context, int id, String identifier) {
+		String qrFormattedString = String.format(Locale.US,"hackillinois://qrcode/user?id=%d&identifier=%s", id, identifier);
+		return QRCode.from(qrFormattedString)
+				.withSize(1024, 1024)
+				.withColor(ContextCompat.getColor(context, R.color.darkPurple), Color.TRANSPARENT)
+				.bitmap();
 	}
 }
