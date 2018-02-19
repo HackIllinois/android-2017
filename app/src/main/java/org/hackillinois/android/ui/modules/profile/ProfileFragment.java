749b7be3d01821d1b8457a7340f32cb521794a02
rohinb2 <bhasin.rohin@gmail.com>
2018-02-19 16:02:30 -0600

    Added QR code functionality.

--- app/src/main/java/org/hackillinois/android/ui/modules/profile/ProfileFragment.java
+++ app/src/main/java/org/hackillinois/android/ui/modules/profile/ProfileFragment.java
@@ -1,6 +1,7 @@
 package org.hackillinois.android.ui.modules.profile;
 
 import android.app.AlertDialog;
+import android.app.Fragment;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
@@ -13,19 +14,27 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 import com.mikepenz.google_material_typeface_library.GoogleMaterial;
 import com.mikepenz.iconics.IconicsDrawable;
 
 import org.hackillinois.android.R;
+import org.hackillinois.android.api.HackIllinoisAPI;
+import org.hackillinois.android.api.response.qrcode.TrackingResponse;
 import org.hackillinois.android.api.response.user.UserResponse;
 import org.hackillinois.android.helper.Settings;
 import org.hackillinois.android.helper.Utils;
 import org.hackillinois.android.ui.base.BaseFragment;
 import org.hackillinois.android.ui.modules.login.LoginChooserActivity;
+
+import java.io.IOException;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -34,12 +43,13 @@ import retrofit2.Callback;
 import retrofit2.Response;
 import timber.log.Timber;
 
-public class ProfileFragment extends BaseFragment {
+public class ProfileFragment extends BaseFragment implements View.OnClickListener {
 	@BindView(R.id.qr_code) ImageView qrCodeImage;
 	@BindView(R.id.user_name) TextView userName;
 	@BindView(R.id.dietary_restrictions) TextView userDietaryRestrictions;
 	@BindView(R.id.profile_refresh) SwipeRefreshLayout swipeRefresh;
 	@BindView(R.id.profile_page_container) View view;
+	@BindView(R.id.event_checkin) Button checkInButton;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -57,6 +67,7 @@ public class ProfileFragment extends BaseFragment {
 		swipeRefresh.setColorSchemeResources(R.color.lightPink);
 
 		setupQrCode();
+		checkInButton.setOnClickListener(this);
 
 		return view;
 	}
@@ -88,6 +99,38 @@ public class ProfileFragment extends BaseFragment {
 		return super.onOptionsItemSelected(item);
 	}
 
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+		IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+		if (result != null) {
+			if (result.getContents() == null) {
+				Toast.makeText(getContext(), "Scan failed", Toast.LENGTH_LONG).show();
+			} else {
+				int id = Integer.valueOf(result.getContents());
+				Call<TrackingResponse> call = getApi().getTracking(Settings.get().getAuthString(), String.valueOf(id));
+				TrackingResponse response = null;
+				try {
+					response = call.execute().body();
+				} catch (IOException e) {
+					System.out.println(e.getMessage());
+				}
+				if (response.getError() == null) {
+					Toast.makeText(getContext(), "Hacker successfully checked in!", Toast.LENGTH_LONG).show();
+				} else {
+					TrackingResponse.Error error = response.getError();
+					if (error.getStatus() == 403) {
+						Toast.makeText(getContext(), "Unauthorized", Toast.LENGTH_LONG).show();
+					} else if (error.getStatus() == 400) {
+						Toast.makeText(getContext(), "Already checked in", Toast.LENGTH_LONG).show();
+					}
+				}
+			}
+
+		} else {
+			super.onActivityResult(requestCode, resultCode, data);
+		}
+	}
+
 	private void logOut() {
 		Timber.i("Logging out current user!");
 		Settings.get().clear(getContext());
@@ -111,6 +154,15 @@ public class ProfileFragment extends BaseFragment {
 				if (response.body() != null) {
 					UserResponse.User data = response.body().getUserResponseData().getUser();
 
+					// Lets staff or admin check in people
+					List<UserResponse.Roles> roles = response.body().getUserResponseData().getRoles();
+					checkInButton.setVisibility(View.INVISIBLE);
+					for (UserResponse.Roles role : roles) {
+						if (role.getRole().equals("ADMIN") || role.getRole().equals("STAFF") || role.getRole().equals("VOLUNTEER")) {
+							checkInButton.setVisibility(View.VISIBLE);
+						}
+					}
+
 					Bitmap qrCode = Utils.getQRCodeBitmap(getContext(), data.getId(), data.getEmail(), 1024);
 					qrCodeImage.setImageBitmap(qrCode);
 					userName.setText(data.getEmail()); // todo actually get name
@@ -127,5 +179,10 @@ public class ProfileFragment extends BaseFragment {
 				stopRefreshing();
 			}
 		});
+	}
+
+	@Override
+	public void onClick(View view) {
+		IntentIntegrator.forSupportFragment(this).initiateScan();
 	}
 }
